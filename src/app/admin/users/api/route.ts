d01085d46cc3235b0b587f1f0455e750dfc0eb60
johnnyltk999 <johnnyltk999@gmail.com>
2024-07-18 22:48:46 +0700

    delete code comment

--- src/app/admin/users/api/route.ts
+++ src/app/admin/users/api/route.ts
@@ -1,23 +1,3 @@
-// import { NextResponse, NextRequest } from "next/server";
-// import { mysqlPool } from "../../../../utils/db";
-
-// export async function GET(req: NextRequest) {
-//   try {
-//     const promissPool = mysqlPool.promise();
-//     const [rows] = await promissPool.execute(`SELECT * FROM  users `);
-
-//     const response = NextResponse.json(rows);
-//     response.headers.set("Cache-Control", "no-store");
-//     return response;
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json(
-//       { error: "Internal Server Error" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
 import { NextResponse, NextRequest } from "next/server";
 import { mysqlPool } from "../../../../../utils/db";
 import bcrypt from "bcryptjs";
